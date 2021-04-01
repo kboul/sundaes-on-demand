e54209a182f878ab4b41ec0d3ce6861ad5a983bf
kboul <kos.voulgaridis@gmail.com>
2021-04-01 13:14:19 +0300

    Write tests for udpating scoops subtotal

--- src/features/entry/Options/index.test.jsx
+++ src/features/entry/Options/index.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Options from '.';
 
@@ -29,4 +30,28 @@ test('displays image for each topping option from server', async () => {
         'M&Ms topping',
         'Hot fudge topping'
     ]);
+});
+
+test('update scoop subtotal when scoops change', async () => {
+    render(<Options optionType="scoops" />);
+
+    // make sure total starts at $0.00
+    const scoopsSubtotal = screen.getByTestId('scoopsTotal');
+    expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+    // update vanilla scoops to 1 and check subtotal
+    const vanillaInput = await screen.findByRole('spinbutton', {
+        name: 'Vanilla'
+    });
+    userEvent.clear(vanillaInput);
+    userEvent.type(vanillaInput, '1');
+    expect(scoopsSubtotal).toHaveTextContent('2.00');
+
+    // update chocolate scoops to 2 and check subtotal
+    const chocolateInput = await screen.findByRole('spinbutton', {
+        name: 'Chocolate'
+    });
+    userEvent.clear(chocolateInput);
+    userEvent.type(chocolateInput, '2');
+    expect(chocolateInput).toHaveTextContent('6.00');
 });
